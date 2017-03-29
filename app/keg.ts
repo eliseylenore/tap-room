7ac8b122cc0595b628f5cc600b20938a83d77861
Elise St Hilaire and Renee Mei <elise+renee@>
2017-03-29 09:25:47 -0700

    set up project folder, add Keg class and AppComponent Class

---
+++ app/keg.ts
@@ -0,0 +1,9 @@
+export class Keg {
+  public pints: number = 124;
+
+  constructor(public name: string,
+    public brand: string,
+    public price: number,
+    public alcoholContent: number
+  ){ }
+}
