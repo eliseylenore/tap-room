c106fd29d3f9060723064cfd0ad5cff84d4bb6cd
Elise St Hilaire and Renee Mei <elise+renee@>
2017-03-29 09:55:49 -0700

    add form to edit name of a keg

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,13 +7,23 @@ import { Keg } from './keg';
   <div class='container'>
     <h1>Tap-Room</h1>
     <div class='panel' *ngFor='let keg of kegs'>
-    <h2>{{keg.name}}</h2>
-    <h3>{{keg.brand}}, \${{keg.price}}/pint</h3>
-    <p>{{keg.alcoholContent}}%, {{keg.pints}} pints</p>
+      <h2>{{keg.name}}</h2>
+      <h3>{{keg.brand}}, \${{keg.price}}/pint</h3>
+      <p>{{keg.alcoholContent}}%, {{keg.pints}} pints</p>
+      <button class="btn btn-info" (click)="editKeg(keg)">Edit</button>
+      <div *ngIf = "selectedKeg.name === keg.name">
+      <input [(ngModel)]="keg.name">
+      </div>
+    </div>
   </div>
   `
 })
 
 export class AppComponent {
   kegs: Keg[] = [new Keg("Awesome Keg", "Renee\'s Brewery", 20, 10), new Keg("Billy Bobs IPA", "Valley Broth", 15, 8), new Keg("Stormy Weather Stout", "Grinil's Place", 8, 5)];
+  selectedKeg: Keg = new Keg();
+
+  editKeg(keg){
+    this.selectedKeg = keg;
+  }
 }
---
+++ app/edit-keg.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+import { Keg } from './keg';
+
+@Component ({
+
+})
+
+export class EditKeg {
+  @Input() keg: Keg;
+}
